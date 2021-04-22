3f4be4dc8bf2b5ff626175ca3ee4bd5b17a91bfe
pena060 <pena060@cougars.csusm.edu>
2021-04-22 14:50:35 -0700

    MAIN PROJECT (CLEAN COPY)

--- app/src/main/java/com/example/kmdb/HomeActivity.kt
+++ app/src/main/java/com/example/kmdb/HomeActivity.kt
@@ -11,25 +11,24 @@ import com.example.kmdb.models.MovieQueryResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.create
 
 //this activity displays the main homepage for the app displaying horror movies as a list
 class HomeActivity : AppCompatActivity(){
-
+    var lManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homeactivitylayout)
         val rv : RecyclerView = findViewById(R.id.rv_movielist)
-        rv.layoutManager = LinearLayoutManager(this)
+        rv.layoutManager = lManager
         rv.setHasFixedSize(true)
-        getMovies { movies : List<Movie> -> rv.adapter = Adapter(movies)}
+        getMovies { movies: List<Movie> -> rv.adapter = Adapter(movies)}
 
 
     }
     //get the movies from the query results
     private fun getMovies(callback: (List<Movie>) -> Unit){
         val apiService = MovieAPIService.getInstance().create(MakeQueryToTMDB::class.java)
-        apiService.getHorrorMovies().enqueue(object : Callback<MovieQueryResponse>{
+        apiService.getHorrorMovies().enqueue(object : Callback<MovieQueryResponse> {
             override fun onResponse(
                 call: Call<MovieQueryResponse>,
                 response: Response<MovieQueryResponse>
